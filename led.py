bc595c1c3084cc0cea97d68b287763d69f1f35de
Gary <garylam5678@gmail.com>
2017-10-23 22:30:05 -0400

    added rgb cycle

--- led.py
+++ led.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 RGB = [18,23,12]
@@ -7,14 +8,31 @@ for pin in RGB:
 	GPIO.output(pin,1)
 
 try:
+	i = 0
 	while(1):
-		request = raw_input("RGB: ")
-		if(len(request)==3):
-			for x in range(3):
-				if(int(request[x])==1):
-					GPIO.output(RGB[x],0)
-				else:
-					GPIO.output(RGB[x],1)
-
+		#request = raw_input("RGB: ")
+		#if(len(request)==3):
+		if(i%7==0):
+			request = [0,0,1]
+		elif(i%7==1):
+			request = [0,1,0]
+		elif(i%7==2):
+			request = [0,1,1]
+		elif(i%7==3):
+			request = [1,0,0]
+		elif(i%7==4):
+			request = [1,0,1]
+		elif(i%7==5):
+			request = [1,1,0]
+		else:
+			request = [1,1,1]
+			
+		for x in range(3):
+			if(int(request[x])==1):
+				GPIO.output(RGB[x],0)
+			else:
+				GPIO.output(RGB[x],1)
+		i = (i+1)%7
+		sleep(.25)
 finally:
 	GPIO.cleanup()
