4ea7dad9f8557685056b3db0b3cde1775a9b2ef3
Gary <garylam5678@gmail.com>
2017-10-23 21:50:46 -0400

    finished code for rgb led

--- led.py
+++ led.py
@@ -1,8 +1,20 @@
-imoprt RPi.GPIO as GPIO
-import time
+import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(13, GPIO.OUT)
-GPIO.output(13, True)
-time.sleep(5)
-GPIO.output(13, False)
+GPIO.setmode(GPIO.BCM)
+RGB = [18,23,12]
+for pin in RGB:
+	GPIO.setup(pin,GPIO.OUT)
+	GPIO.output(pin,1)
+
+try:
+	while(1):
+		request = raw_input("RGB: ")
+		if(len(request)==3):
+			for x in range(3):
+				if(int(request[x])==1):
+					GPIO.output(RGB[x],0)
+				else:
+					GPIO.output(RGB[x],1)
+
+finally:
+	GPIO.cleanup()
